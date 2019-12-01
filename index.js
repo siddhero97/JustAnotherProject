e693296906bcf3d1d4ce1b94e6858b21855d418b
rayhua12 <45476725+rayhua12@users.noreply.github.com>
2019-11-30 18:17:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post('/webhook', (req, res) => {
 
             } else {
                 if(users[senderPsid].getrec() == true){
-                    addHobbies(message.text, users[senderPsid]);
+                    addHobbies(webhookEvent.message.text, users[senderPsid]);
                     console.log(users[senderPsid].getHobbies());
                     let temp = compareUsers(user1);
                     // makePair(user1, temp)
